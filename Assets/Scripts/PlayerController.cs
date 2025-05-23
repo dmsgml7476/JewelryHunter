8100228d93971005531218e92f1da1b9b2ed9fff
dmsgml7476 <dmsgml7476@gmail.com>
2025-05-23 17:45:17 +0900

    0523 3일차 끝

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,10 +14,14 @@ public class PlayerController : MonoBehaviour
     bool goJump = false;
     bool onGround = false; // onGround : 땅에 있는지 여부를 의미.
 
+    // 이중 점프 - 시작 박은희 2023.10.16
+    int jumpCount = 0; // 점프 횟수 초기화
+    public int maxJumpCount = 2; // 최대 점프 횟수
+
 
     //애니메이션
 
-    Animator animator;
+    Animator animator;  // 애니메이션을 제어하는 컴포넌트. 애니메이션을 제어하기 위한 변수.
     public string stopAnime = "PlayerStop";
     public string moveAnime = "PlayerMove";
     public string jumpAnime = "PlayerJump";
@@ -38,14 +42,14 @@ public class PlayerController : MonoBehaviour
         nowAnime = stopAnime;
         oldAnime = stopAnime;
 
-        gameState = "playing"; // 게임 중
+        gameState = "Playing"; // 게임 중
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        if (gameState == "playing")
+        if (gameState != "Playing")
         {
             return;
         }
@@ -70,38 +74,71 @@ public class PlayerController : MonoBehaviour
             Jump(); // jump가 true면 점프를 의미.
         }
 
-        
+
     }
 
     // FixedUpdate는 물리 연산을 위한 메소드로, 매 프레임마다 호출됨
     void FixedUpdate()
     {
-        if (gameState != "playing")
+        if (gameState != "Playing")
         {
             return;
         }
-        // 착지 판정
-        onGround = Physics2D.Linecast(transform.position, transform.position - (transform.up * 0.1f), groundLayer); // Linecast : 두 점을 연결하는 선을 그리는 메소드. transform.position : 현재 오브젝트의 위치를 의미. transform.up : 오브젝트의 위쪽 방향을 의미. 0.1f : 0.1만큼 아래로 이동.
-      
-        if(onGround || axisH != 0)
+        //// 착지 판정 코드 1
+        //onGround = Physics2D.Linecast(transform.position, transform.position - (transform.up * 0.1f), groundLayer); // Linecast : 두 점을 연결하는 선을 그리는 메소드. transform.position : 현재 오브젝트의 위치를 의미. transform.up : 오브젝트의 위쪽 방향을 의미. 0.1f : 0.1만큼 아래로 이동.
+        //// 타겟지점과 포지션의 거리(transform.position - (transform.up * 0.1f))를 비교하여 땅에 닿아있는지 확인하는 것.
+        //// transform.up : 오브젝트의 위쪽 방향을 의미. 0.1f : 0.1만큼 아래로 이동.
+        //// 단위벡터(1과 0으로 이루어져서 방향만 존재하는 벡터) * speed: 진짜 벡터(방향과 크기를 모두 가진 벡터)로 변환하는 것.
+
+
+        // 착지 판정 코드 2
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 0.1f, groundLayer); // RaycastHit2D : 레이캐스트의 충돌 정보를 저장하는 구조체. Physics2D.Raycast : 레이캐스트를 쏘는 메소드. transform.position : 현재 오브젝트의 위치를 의미. transform.down : 오브젝트의 아래쪽 방향을 의미. 0.1f : 0.1만큼 아래로 이동.
+        onGround = hit.collider != null;
+        // hit.collider : 레이캐스트가 충돌한 콜라이더를 의미. != null : 충돌한 콜라이더가 없으면 false, 있으면 true를 의미.
+        // 충돌한 콜라이더가 무엇인지를 알려줄 수 있어서 활용도가 더 높다.
+
+        if (onGround || axisH != 0)
         {
             // 속도 갱신하기
             rbody.velocity = new Vector2(speed * axisH, rbody.velocity.y); // rbody : Rigidbody2D형 변수. velocity : 속도를 의미. new Vector2 : 2차원 벡터를 생성하는 메소드. axisH * 3.0f : 수평축의 속도를 3배로 증가시키는 것. rbody.velocity.y : y축의 속도.
             // y의 값은 여기서 조정하지 않고 다른곳에서 적용하는 y벡터값을 그대로 사용하라는 뜻
             // y는 기존값 유지!!!
-        }
-        if(onGround && goJump)
-        {
 
-            Debug.Log("점프");
-            Vector2 jumpPw = new Vector2(0, jump);
-            rbody.AddForce(jumpPw, ForceMode2D.Impulse);
-            goJump = false;
+        }
+        //if (onGround && goJump)
+        //{
+
+        //    Debug.Log("점프");
+        //    Vector2 jumpPw = new Vector2(0, jump);
+        //    rbody.AddForce(jumpPw, ForceMode2D.Impulse);
+        //    goJump = false;
+        //}
+
+        // 이중점프
+
+        if (onGround)
+        {
+            jumpCount = 0;
+        }
+
+
+        //if (goJump && jumpCount < maxJumpCount)
+        //{
+        //    rbody.AddForce(new Vector2(0, jump), ForceMode2D.Impulse);
+        //    goJump = false; // 점프가 끝나면 goJump를 false로 바꿈
+        //    jumpCount++; // 점프 횟수 증가
+        //}
+
+        if (goJump)
+        {
+            rbody.AddForce(new Vector2(0, jump), ForceMode2D.Impulse);
+            goJump = false; // 점프가 끝나면 goJump를 false로 바꿈
         }
 
         if (onGround)
         {
-            if(axisH == 0)
+            if (axisH == 0)
             {
                 nowAnime = stopAnime;
             }
@@ -114,42 +151,85 @@ public class PlayerController : MonoBehaviour
         {
             nowAnime = jumpAnime;
         }
+        //animator.Play(nowAnime);
 
         if (nowAnime != oldAnime)
         {
             oldAnime = nowAnime;
             animator.Play(nowAnime);
         }
-        
+
     }
 
     public void Jump()
     {
-        goJump = true;
-        Debug.Log("점프버튼 눌림!");
+        //goJump = true;
+        //Debug.Log("점프버튼 눌림!");
+
+        jumpCount++; // 점프 횟수 증가
+        if (jumpCount < maxJumpCount)
+        {
+            goJump= true; // 점프를 의미.
+        }
+        
     }
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.tag == "Goal")
+        if (collision.gameObject.tag == "Goal")
         {
             Goal();
         }
-        else if(collision.gameObject.tag == "Dead")
+        else if (collision.gameObject.tag == "Dead")
         {
             GameOver();
         }
 
     }
 
+    // 위에는 isTrigger를 사용한 충돌 처리 코드입니다.
+    // 아래는 isTrigger가 아닌 일반적인 충돌 처리도 가능하도록 추가한 코드입니다.
+    // 충돌 감지와 더불어 이동을 막아줌. 하지만 비쌈. 시스템에 부하를 줌
+
+    //public void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    Debug.Log("OnCollisionEnter2D 충돌 이벤트 발생");
+    //    if (collision.gameObject.tag == "Goal")
+    //    {
+    //        Goal();
+    //    }
+    //    else if (collision.gameObject.tag == "Dead")
+    //    {
+    //        GameOver();
+    //    }
+    //}
+
     public void Goal()
     {
         animator.Play(goalAnime);
+        gameState = "gameclear"; // 게임 클리어
+        GameStop(); // 게임 정지
     }
 
     public void GameOver()
     {
         animator.Play(deadAnime); 
+
+        gameState = "gameover";
+        GameStop(); // 게임 정지
+
+        // 게임 오버 연출
+
+        GetComponent<CapsuleCollider2D>().enabled = false;
+
+        rbody.AddForce(new Vector2(0, 5), ForceMode2D.Impulse);
+    }
+
+    void GameStop()
+    {
+        //Rigidbody2D 가져오기
+        Rigidbody2D rbody = GetComponent<Rigidbody2D>();
+        rbody.velocity = new Vector2(0, 0);
     }
 }
 
